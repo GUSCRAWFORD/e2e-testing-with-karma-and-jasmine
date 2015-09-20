bb654023769f35aa2f5e2cc92416df71ff74a14a
GUSCRAWFORD <crawford.gus@gmail.com>
2015-09-20 14:22:53 -0400

    Added skeleton C# web-application

---
+++ sample-project/sample-project.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace sample_project.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
