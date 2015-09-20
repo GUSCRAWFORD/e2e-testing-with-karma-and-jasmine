b8438ac604b4910ecec83c95e55856d20d1bc2da
GUSCRAWFORD <crawford.gus@gmail.com>
2015-09-20 17:25:36 -0400

    Intialize Jasmine Framework

---
+++ jasmine-testing/spec/user.service.spec.js
@@ -0,0 +1,7 @@
+﻿describe("user", function () {
+    beforeEach(module("app"));
+ 
+    it(".identify returns false with no data passed to it", inject(function (user) {
+        expect(user.identify()).toEqual(false);
+    }))
+});
--- spec/user.service.spec.js
+++
@@ -1,5 +0,0 @@
-﻿describe("user", function () {
-    it(".identify returns false with no data passed to it", function () {
-        expect(user.identify()).toEqual(false);
-    })
-});
